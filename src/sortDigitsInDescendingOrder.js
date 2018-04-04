48c496ea06c350aec7deacc2f654521bc10c18d0
Sim Tze Jan <tzejan@gmail.com>
2018-04-04 10:37:05 +0800

    Added solution for sortDigitsInDescendingOrder

--- src/sortDigitsInDescendingOrder.js
+++ src/sortDigitsInDescendingOrder.js
@@ -3,7 +3,11 @@ as a argument and return it with its digits in descending order.
 Essentially, rearrange the digits to create the highest possible number. */
 
 const sortDigitsInDescendingOrder = (digits) => {
-
+    const arrayOfStrings = digits.toString().split('');
+    arrayOfStrings.sort((a,b) => {
+        return b-a;
+    });
+    return parseInt(arrayOfStrings.join(''));
 }
 
 module.exports = sortDigitsInDescendingOrder
